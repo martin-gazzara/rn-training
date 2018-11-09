e433dddb4ec5fdbf3a30e7d3675c923b4d63bcee
Martin Gazzara <martingazzara@gmail.com>
2018-11-09 16:26:01 -0300

    Exercise II, Topic 1

--- training-playground/App.js
+++ training-playground/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import { Alert, StyleSheet, Text, View, TouchableHighLight } from 'react-native';
-import Box from "../training-playground/components/topic1"
+import Exercise from "../training-playground/components/topic1"
 
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <Box style={styles.box}/>        
+      <Exercise/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  box:{
-      width:200,
-      height:200,
-      position:"absolute",
-      left:50,
-      top:100,
-      borderWidth:3,
-      borderColor:"violet",
-      borderStyle:"dotted",
-      borderRadius:10,
-      backgroundColor:"blue"
-    }
-})
-
--- training-playground/components/topic1.js
+++ training-playground/components/topic1.js
@@ -1,13 +1,44 @@
 import React from "react";
-import {View} from "react-native";
+import {View, StyleSheet} from "react-native";
 
-export default class Box extends React.Component{
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    box1:{
+        width:50,
+        height:50,
+        backgroundColor:"blue",
+        borderRadius:10
+    },
+    box2:{
+        width:100,
+        height:100,
+        backgroundColor:"red",
+        borderRadius:10
+    },
+    box3:{
+        width:150,
+        height:150,
+        backgroundColor:"yellow",
+        borderRadius:10
+    }
+})
+
+export default class Exercise extends React.Component{
+
+    
 
     render(){
         return(
-            <View style={this.props.style}></View>
+            <View style={styles.container}>
+                <View style={styles.box1}/>
+                <View style={styles.box2}/>
+                <View style={styles.box3}/>
+            </View>
        )
     }
 
 }
+
 
