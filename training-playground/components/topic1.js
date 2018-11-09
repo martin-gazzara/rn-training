d6e9cc64cd30036883d42b53e362d6eba5ad57c5
Martin Gazzara <martingazzara@gmail.com>
2018-11-09 20:53:26 -0300

    Exercise VI, topic 1

--- training-playground/components/topic1.js
+++ training-playground/components/topic1.js
@@ -3,19 +3,17 @@ import {View, StyleSheet, Image} from "react-native";
 
 const styles = StyleSheet.create({
     container:{
-        flex:1        
-    },
-    image1:{
+        width:250,
+        height:250,
         position:"absolute",
-        top:50,
+        top:100,
         left:50,
-        width:100,
-        height:100
+        borderWidth:2,
+        borderColor:"black"
     },
-    image2:{
-        position:"absolute",
-        top:210,
-        left:50
+    img:{
+        flex:1,
+        resizeMode:"contain"
     }
 })
 
@@ -26,14 +24,7 @@ export default class Exercise extends React.Component{
     render(){
         return(
         <View style={styles.container}>
-            <Image
-                style={styles.image1}
-                source={{uri: 'http://moziru.com/images/pixel-clipart-mario-mushroom-5.jpg'}}
-            />
-            <Image
-                style={styles.image2}
-                source={require('../images/mario.png')}
-            />
+            <Image style={styles.img} source={require("../images/mario.png")}/>
         </View>
        )
     }
