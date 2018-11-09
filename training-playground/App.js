243483b1208816bf217a7bfe8da6f31aba32a7c7
Martin Gazzara <martingazzara@gmail.com>
2018-11-09 15:58:20 -0300

    Exercise I

--- training-playground/App.js
+++ training-playground/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, TouchableHighLight } from 'react-native';
+import Box from "../training-playground/components/topic1"
+
 
 export default class App extends React.Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Box style={styles.box}/>        
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  box:{
+      width:200,
+      height:200,
+      position:"absolute",
+      left:50,
+      top:100,
+      borderWidth:3,
+      borderColor:"violet",
+      borderStyle:"dotted",
+      borderRadius:10,
+      backgroundColor:"blue"
+    }
+})
+
