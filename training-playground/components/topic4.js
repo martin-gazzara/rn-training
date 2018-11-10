bed7fbeb47c19c30cbcc8df3a8a9a299b030f677
Martin Gazzara <martingazzara@gmail.com>
2018-11-10 18:39:00 -0300

    Exercise II, topic 4

--- training-playground/components/topic4.js
+++ training-playground/components/topic4.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {View,Text} from "react-native";
-import {createStackNavigator} from "react-navigation";
+import {createStackNavigator, createBottomTabNavigator} from "react-navigation";
 
 class HomeScreen extends React.Component{
 
@@ -15,9 +15,30 @@ class HomeScreen extends React.Component{
 
 }
 
-const RootStack = createStackNavigator({
+class SecondScreen extends React.Component{
+
+    render(){
+        return(
+            <View style={{Flex:1,backgroundColor:"#cc0000",justifyContent:"center"}}>
+                <Text style={{color:"#fff"}}>This is the second screen</Text>
+            </View>
+        )
+    }
+}
+
+const HomeStack = createStackNavigator({
     Home:HomeScreen
 });
+
+const SecondStack = createStackNavigator({
+    Second:SecondScreen
+})
+
+const RootStack = createBottomTabNavigator({
+    Home:HomeStack,
+    Second:SecondStack
+})
+
 
 export default RootStack;
 
