e3045d90d962281eb051e61f4a7d4655c2f2dcc8
Martin Gazzara <martingazzara@gmail.com>
2018-11-10 00:31:36 -0300

    Exercise IV, topic 2

--- training-playground/components/topic2.js
+++ training-playground/components/topic2.js
@@ -2,17 +2,14 @@ import React from "react";
 import {View} from "react-native";
 import styles from "../styles/styleTopic2";
 
-const changeDirection = false;   // If you want to see the boxes in a row, you must to set this in 'true'. If this is false
-                                 // the boxes will be in a column
-
 export default class Exercise extends React.Component{
 
     render(){
         return(
-            <View style={[styles.container,change ? styles.row : styles.col]}>
-                <View style={styles.box1}></View>
-                <View style={styles.box2}></View>
-                <View style={styles.box3}></View>
+            <View style={styles.container}>
+                  <View style={styles.first}></View>
+                  <View style={styles.second}></View>
+                  <View style={styles.third}></View>
             </View>
         )
     }
--- training-playground/styles/styleTopic2.js
+++ training-playground/styles/styleTopic2.js
@@ -1,39 +1,34 @@
-import {StyleSheet} from "react-native";
+import {StyleSheet,Dimensions} from "react-native";
+
+const aux = Dimensions.get("screen");
 
 const styles = StyleSheet.create({
     container:{
         flex:1,
         justifyContent:"space-between",
         backgroundColor:"#7fbfbf",
-        padding:10
+        padding:100
     },
-    box1:{
-        width:100,
-        height:100,
+    first:{
+        width:(aux.width*1/2),
+        height:(aux.width*1/2),
         backgroundColor:"#e50202",
         marginBottom:10
     },
-    box2:{
+    second:{
         marginBottom:10,
-        width:100,
-        height:100,
+        width:(aux.width*3/10),
+        height:(aux.width*3/10),
         backgroundColor:"#2b0057",
         
     },
-    box3:{
+    third:{
         marginBottom:10,
-        width:100,
-        height:100,
+        width:(aux.width*1/5),
+        height:(aux.width*1/5),
         backgroundColor:"#ffffff",
         
-    },
-    row:{
-        flexDirection:"row"
-    },
-    col:{
-        flexDirection:"column"
     }
-
 })
 
 export default styles;
